df9e383ae6cc352f3a351b3c4190a9a2aa952c2f
Doniyor Sayfullaev <dima.sayfullaev@mail.ru>
2022-07-01 00:20:32 +0500

    feature: Added tables for client, organization & bills

--- src/apps/core/models.py
+++ src/apps/core/models.py
@@ -1,3 +1,67 @@
 from django.db import models
 
-# Create your models here
+
+class Client(models.Model):
+    """
+        Модель Клиента
+    """
+    name = models.CharField(
+        verbose_name='Имя', max_length=255, unique=True
+    )
+
+    class Meta:
+        verbose_name = 'Клиент'
+        verbose_name_plural = 'Клиенты'
+
+    def __str__(self):
+        return self.name
+
+
+class Organization(models.Model):
+    """
+        Модель организации
+    """
+    client = models.ForeignKey(
+        to=Client, on_delete=models.CASCADE, related_name='organizations'
+    )
+    name = models.CharField(
+        verbose_name='Имя', max_length=255
+    )
+    address = models.CharField(
+        verbose_name='Адрес', max_length=255
+    )
+
+    class Meta:
+        unique_together = ('client', 'name')
+        verbose_name = 'Организация'
+        verbose_name_plural = 'Организации'
+
+    def __str__(self):
+        return self.name
+
+
+class Bill(models.Model):
+    client = models.ForeignKey(
+        verbose_name='Клиент', to=Client, on_delete=models.CASCADE, related_name='bills'
+    )
+    organization = models.ForeignKey(
+        verbose_name='Организация', to=Organization, on_delete=models.CASCADE, related_name='bills'
+    )
+    number = models.IntegerField(verbose_name='№')
+    sum = models.IntegerField(verbose_name='Сумма')
+    date = models.DateField(verbose_name='Дата')
+    service = models.CharField(verbose_name='Сервис', max_length=255)
+    fraud_score = models.DecimalField(
+        verbose_name='Детектор мошенничества',
+        max_digits=3, decimal_places=2
+    )
+    service_class = models.IntegerField(verbose_name='Классификатор услуг: Класс')
+    service_name = models.CharField(verbose_name='Классификатор услуг: Имя', max_length=255)
+
+    class Meta:
+        unique_together = ('organization', 'number')
+        verbose_name = 'Счета организации клиента'
+        verbose_name_plural = 'Счета организации клиента'
+
+    def __str__(self):
+        return f'{self.organization} - {self.number}'
